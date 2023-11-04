53affd173ab5c08b7831d53f6ae297adc0050570
farida2498 <125817327+faridaheydarova@users.noreply.github.com>
2023-11-04 22:27:23 +0400

    step-1
    proyekt yaradildi

---
+++ src/test/java/fashion/trendaz/fashiontrendazproject/FashionTrendazProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package fashion.trendaz.fashiontrendazproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FashionTrendazProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
